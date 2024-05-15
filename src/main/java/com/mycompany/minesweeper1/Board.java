58e493bb1c30ae867c64b28386a6f844dafa042e
lolmlg1 <Baldie1444@gmail.com>
2024-05-15 13:30:27 -0400

    First commit

---
+++ src/main/java/com/mycompany/minesweeper1/Board.java
@@ -0,0 +1,5 @@
+package com.mycompany.minesweeper1;
+
+public class Board {
+    
+}
---
+++ src/main/java/com/mycompany/minesweeper1/Minesweeper.java
@@ -0,0 +1,11 @@
+package com.mycompany.minesweeper1;
+
+public class Minesweeper {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
