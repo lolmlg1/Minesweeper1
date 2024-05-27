b55e9ea338900e1d9f474d79506d46dbbb263db8
jamar5118 <jamar5118@HT0KRP2.Student.UGDSB.ED>
2024-05-27 13:26:13 -0400

    added Minesweeper code

--- src/main/java/com/mycompany/minesweeper1/Board.java
+++ src/main/java/com/mycompany/minesweeper1/Board.java
@@ -1,5 +1,246 @@
 package com.mycompany.minesweeper1;
 
-public class Board {
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+
+    private final int NUM_IMAGES = 13;
+    private final int CELL_SIZE = 15;
+
+    private final int COVER_FOR_CELL = 10;
+    private final int MARK_FOR_CELL = 10;
+    private final int EMPTY_CELL = 0;
+    private final int MINE_CELL = 9;
+    private final int COVERED_MINE_CELL = MINE_CELL + COVER_FOR_CELL;
+    private final int MARKED_MINE_CELL = COVERED_MINE_CELL + MARK_FOR_CELL;
+
+    private final int DRAW_MINE = 9;
+    private final int DRAW_COVER = 10;
+    private final int DRAW_MARK = 11;
+    private final int DRAW_WRONG_MARK = 12;
+
+    private final int N_MINES = 40;
+    private final int N_ROWS = 16;
+    private final int N_COLS = 16;
+
+    private final int BOARD_WIDTH = N_COLS * CELL_SIZE + 1;
+    private final int BOARD_HEIGHT = N_ROWS * CELL_SIZE + 1;
+
+    private int[] field;
+    private boolean inGame;
+    private int minesLeft;
+    private Image[] img;
+
+    private int allCells;
+    private final JLabel statusbar;
+
+    public Board(JLabel statusbar) {
+
+        this.statusbar = statusbar;
+        initBoard();
     
 }
+
+    private void initBoard() {
+
+        setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
+
+        img = new Image[NUM_IMAGES];
+
+        for (int i = 0; i < NUM_IMAGES; i++) {
+
+            var path = "C:\\Users\\jamar5118\\Documents\\minesweeper\\src\\" + i + ".png";
+            img[i] = (new ImageIcon(path)).getImage();
+        }
+
+        addMouseListener(new MinesAdapter());
+        newGame();
+    }
+
+    private void newGame() {
+
+        int cell;
+
+        var random = new Random();
+        inGame = true;
+        minesLeft = N_MINES;
+
+        allCells = N_ROWS * N_COLS;
+        field = new int[allCells];
+
+        for (int i = 0; i < allCells; i++) {
+
+            field[i] = COVER_FOR_CELL;
+        }
+
+        statusbar.setText(Integer.toString(minesLeft));
+
+        int i = 0;
+
+        while (i < N_MINES) {
+
+            int position = (int) (allCells * random.nextDouble());
+
+            if ((position < allCells)
+                    && (field[position] != COVERED_MINE_CELL)) {
+
+                int current_col = position % N_COLS;
+                field[position] = COVERED_MINE_CELL;
+                i++;
+
+                if (current_col > 0) {
+                    cell = position - 1 - N_COLS;
+                    if (cell >= 0) {
+                        if (field[cell] != COVERED_MINE_CELL) {
+                            field[cell] += 1;
+                        }
+                    }
+                    cell = position - 1;
+                    if (cell >= 0) {
+                        if (field[cell] != COVERED_MINE_CELL) {
+                            field[cell] += 1;
+                        }
+                    }
+
+                    cell = position + N_COLS - 1;
+                    if (cell < allCells) {
+                        if (field[cell] != COVERED_MINE_CELL) {
+                            field[cell] += 1;
+                        }
+                    }
+                }
+
+                cell = position - N_COLS;
+                if (cell >= 0) {
+                    if (field[cell] != COVERED_MINE_CELL) {
+                        field[cell] += 1;
+                    }
+                }
+
+                cell = position + N_COLS;
+                if (cell < allCells) {
+                    if (field[cell] != COVERED_MINE_CELL) {
+                        field[cell] += 1;
+                    }
+                }
+
+                if (current_col < (N_COLS - 1)) {
+                    cell = position - N_COLS + 1;
+                    if (cell >= 0) {
+                        if (field[cell] != COVERED_MINE_CELL) {
+                            field[cell] += 1;
+                        }
+                    }
+                    cell = position + N_COLS + 1;
+                    if (cell < allCells) {
+                        if (field[cell] != COVERED_MINE_CELL) {
+                            field[cell] += 1;
+                        }
+                    }
+                    cell = position + 1;
+                    if (cell < allCells) {
+                        if (field[cell] != COVERED_MINE_CELL) {
+                            field[cell] += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private void find_empty_cells(int j){
+        int current_col = j % N_COLS;
+        int cell;
+
+        if (current_col > 0) {
+            cell = j - N_COLS - 1;
+            if (cell >= 0) {
+                if (field[cell] > MINE_CELL) {
+                    field[cell] -= COVER_FOR_CELL;
+                    if (field[cell] == EMPTY_CELL) {
+                        find_empty_cells(cell);
+                    }
+                }
+            }
+
+            cell = j - 1;
+            if (cell >= 0) {
+                if (field[cell] > MINE_CELL) {
+                    field[cell] -= COVER_FOR_CELL;
+                    if (field[cell] == EMPTY_CELL) {
+                        find_empty_cells(cell);
+                    }
+                }
+            }
+
+            cell = j + N_COLS - 1;
+            if (cell < allCells) {
+                if (field[cell] > MINE_CELL) {
+                    field[cell] -= COVER_FOR_CELL;
+                    if (field[cell] == EMPTY_CELL) {
+                        find_empty_cells(cell);
+                    }
+                }
+            }
+        }
+
+        cell = j - N_COLS;
+        if (cell >= 0) {
+            if (field[cell] > MINE_CELL) {
+                field[cell] -= COVER_FOR_CELL;
+                if (field[cell] == EMPTY_CELL) {
+                    find_empty_cells(cell);
+                }
+            }
+        }
+
+        cell = j + N_COLS;
+        if (cell < allCells) {
+            if (field[cell] > MINE_CELL) {
+                field[cell] -= COVER_FOR_CELL;
+                if (field[cell] == EMPTY_CELL) {
+                    find_empty_cells(cell);
+                }
+            }
+        }
+
+        if (current_col < (N_COLS - 1)) {
+            cell = j - N_COLS + 1;
+            if (cell >= 0) {
+                if (field[cell] > MINE_CELL) {
+                    field[cell] -= COVER_FOR_CELL;
+                    if (field[cell] == EMPTY_CELL) {
+                        find_empty_cells(cell);
+                    }
+                }
+            }
+
+            cell = j + N_COLS + 1;
+            if (cell < allCells) {
+                if (field[cell] > MINE_CELL) {
+                    field[cell] -= COVER_FOR_CELL;
+                    if (field[cell] == EMPTY_CELL) {
+                        find_empty_cells(cell);
+                    }
+                }
+            }
+
+            cell = j + 1;
+            if (cell < allCells) {
+                if (field[cell] > MINE_CELL) {
+                    field[cell] -= COVER_FOR_CELL;
+                    if (field[cell] == EMPTY_CELL) {
+                        find_empty_cells(cell);
+                    }
+                }
+            }
+        }
+    }
